df7353f2fd9fad7c48f2488d5bd34341d2d1d1a3
huuhieu1011 <01643815296h@gmail.com>
2021-07-10 21:13:36 +0700

    done crud api

--- db/index.js
+++ db/index.js
@@ -1,11 +1,13 @@
 
 const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost:27017/ManageBook', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-})
-    .then(res => console.log('Connect successfully'))
-    .catch(err => console.log('Connect Failure'))
+try {
+    mongoose.connect('mongodb://localhost:27017/ManageBook', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
+    console.log('Connect successfully')
+} catch (err) {
+    console.log('Connect Failure')
+}
--- routes/api.js
+++ routes/api.js
@@ -4,27 +4,47 @@ const router = express.Router()
 
 const Book = require('../models/Book')
 
-//create a book
+// create a book
+// api/book/addnew
 router.post('/book/addnew', async (req, res) => {
-    const book = req.body
-    await Book.create(book)
-        .then(book => res.status(201).send(book))
-        .catch(err => res.status(400).send(err))
+    await Book.create(req.body)
+        .then(book => res.status(201).json(book))
+        .catch(err => res.status(500).json(err))
 })
 
-//get all book
-router.get('/book/list', async (req, res) => {
+// get all book
+// api/book
+router.get('/book', async (req, res) => {
     await Book.find({})
-        .then(book => res.status(200).send(book))
-        .catch(err => res.status(400).send(err))
+        .then(book => res.status(200).json(book))
+        .catch(err => res.status(500).json(err))
 })
 
-//get by id
+// get by id
+// api/book/:id
 router.get('/book/:id', async (req, res) => {
     const id = req.params.id
     await Book.findById(id)
-        .then(book => res.status(200).send(book))
-        .catch(err => res.status(404).send(err))
+        .then(book => res.status(200).json(book))
+        .catch(err => res.status(500).json(err))
+})
+
+// update
+// api/book/update/:id
+router.put('/book/update/:id', async (req, res) => {
+    const id = req.params.id
+    await Book.findByIdAndUpdate(id, req.body)
+        .then(() => res.status(200).json('update successfully'))
+        .catch(err => res.status(500).json(err))
+})
+
+// delete
+// api/book/delete/:id
+router.delete('/book/delete/:id', async (req, res) => {
+    const id = req.params.id
+    await Book.findByIdAndDelete(id)
+        .then(() => res.status(200).json('delete successfully'))
+        .catch(err => res.status(500).json(err))
 })
 
 module.exports = router
